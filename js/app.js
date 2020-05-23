4c0d757cda3b2ad0913b2bbfebbb93160d133d17
Sameer <itsmdsameerkhan@gmail.com>
2020-05-23 18:51:22 +0545

    Winner's Score upleveled to 100

--- js/app.js
+++ js/app.js
@@ -29,7 +29,7 @@ document.querySelector(".btn-hold").addEventListener("click", function () {
       scores[activePlayer];
 
     // Check if player won.
-    if (scores[activePlayer] >= 10) {
+    if (scores[activePlayer] >= 100) {
       document.querySelector("#name-" + activePlayer).textContent = "WINNER!";
       document.querySelector(".dice").style.display = "none";
 
